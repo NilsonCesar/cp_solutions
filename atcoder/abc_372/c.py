7c7f9a8339c1d6f3f22bc19f6f00bdfc349ba2c9
Nilson Cesar <admncoaf@gmail.com>
2024-09-21 10:53:47 -0300

    Add c solution to abc_372

--- atcoder/abc_372/c.py
+++ atcoder/abc_372/c.py
@@ -1,11 +1,27 @@
 n, q = map(int, input().split())
-s = input()
-s_arr = []
-for c in s:
-    s_arr.append(c)
-print(s_arr, str(s_arr))
+s = list(input())
+ans = 0
 
-for i in range(q):
+for i in range(n - 2):
+    if s[i] == "A" and s[i + 1] == "B" and s[i + 2] == "C":
+        ans += 1
+
+for _ in range(q):
     x, c = input().split()
-    s_arr[int(x) - 1] = c
-    print(str(s_arr).count("ABC"))
+    x = int(x) - 1
+
+    for i in range(3):
+        idx = x - i
+        if idx >= 0 and idx < n - 2:
+            if s[idx] == "A" and s[idx + 1] == "B" and s[idx + 2] == "C":
+                ans -= 1
+    
+    s[x] = c
+
+    for i in range(3):
+        idx = x - i
+        if idx >= 0 and idx < n - 2:
+            if s[idx] == "A" and s[idx + 1] == "B" and s[idx + 2] == "C":
+                ans += 1
+
+    print(ans)
