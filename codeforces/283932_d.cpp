2f14c2a3a7480b1da89347416bf8c6428ef07d00
nilsoncesar <admncoaf@gmail.com>
2025-06-27 12:36:06 -0300

    Aquilo que parecia, impossível, aquilo que parecia, não ter saííídaaa

--- codeforces/283932_d.cpp
+++ codeforces/283932_d.cpp
@@ -9,7 +9,42 @@ void mainIO(string s = "") {
     }
 }
 
+long long calc_ballons(long long t, long long z, long long y, long long T) {
+    return  T / (y + z * t) * z + min(z * t, T % (y + z * t)) / t;
+}
+
 int main(void) {
     mainIO();
+    long long n, m, ans, l = 0, r = 1e9, mid, tmp_b;
+    cin >> m >> n;
+    vector<vector<long long>> a(n, vector<long long>(3));
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < 3; j++)
+            cin >> a[i][j];
+
+    while (l <= r) {
+        mid = l + (r - l) / 2;
+        tmp_b = 0;
+        for (int i = 0; i < n; i++) 
+            tmp_b += calc_ballons(a[i][0], a[i][1], a[i][2], mid);
+        
+        if (tmp_b >= m) {
+            ans = mid;
+            r = mid - 1;
+        } else {
+            l = mid + 1;
+        }
+    }
+
+    cout << ans << '\n';
+    long long remaining = m;
+    for (int i = 0; i < n; i++) {
+        long long b = calc_ballons(a[i][0], a[i][1], a[i][2], ans);
+        if (remaining < b) cout << remaining;
+        else cout << b;
+        cout << ' ';
+        remaining -= min(b, remaining);
+    }
+    cout << '\n';
     return 0;
 }
