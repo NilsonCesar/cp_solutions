434cc784e9f7c40bb1d02fb7ddc154a4fd19ba0e
nilsoncesar <admncoaf@gmail.com>
2025-11-16 17:48:54 -0300

    Change template

--- 0_templates/template.cpp
+++ 0_templates/template.cpp
@@ -1,16 +1,10 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-void mainIO(string s = "") {
-    cin.tie(NULL); ios_base::sync_with_stdio(false);
-    if(s.size()) {
-        freopen((s + ".in").c_str(), "r", stdin);
-        freopen((s + ".out").c_str(), "w", stdout);
-    }
-}
 
-int main(void) {
-    mainIO();
+#define _ cin.tie(NULL); ios_base::sync_with_stdio(false);
+
+int main(void) {_
 
     return 0;
 }
