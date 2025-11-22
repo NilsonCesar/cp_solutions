6f2721907e1bd5c485d8c687823e3d6559e3b817
nilsoncesar <admncoaf@gmail.com>
2025-11-22 15:33:08 -0300

    [cses] solve coin combinations I

---
+++ cses/dynamic_programming/coin_combinations_i.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define _ cin.tie(NULL); ios_base::sync_with_stdio(false);
+#define INF 0x3f3f3f3f
+#define MAXN 1000007
+
+long long memo[MAXN], c[MAXN];
+
+int main(void) {_
+    int n, x;
+    cin >> n >> x;
+    for (int i = 0; i < n; i++) cin >> c[i];
+
+    memo[0] = 1;
+    for (int s = 1; s <= x; s++) {
+        for (int i = 0; i < n; i++) {
+            if (s - c[i] < 0) continue;
+            memo[s] += (memo[s - c[i]] % (1000000000+7));
+        }
+    }
+
+    cout << memo[x] % (1000000000+7) << '\n';
+
+    return 0;
+}
