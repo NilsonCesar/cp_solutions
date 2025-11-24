8ad3e0e1d27e153bd195167b1a6329b053514dc0
nilsoncesar <admncoaf@gmail.com>
2025-11-23 21:39:02 -0300

    [maratona subreg 2024] resolve h

--- maratona/2024/fase_regional/h.cpp
+++ maratona/2024/fase_regional/h.cpp
@@ -2,59 +2,50 @@
 
 using namespace std;
 
-string m, n;
-vector<int> all_n;
-int M, N, ans = -1;
+string m, n, resp;
 
-void make_all_n(int i, int cur_n) {
-    if (i == N) {
-        all_n.push_back(cur_n);
-        return;
-    }
-
-    if (n[i] != '*') make_all_n(i + 1, (cur_n << 1) | (n[i] - '0'));
-    else {
-        make_all_n(i + 1, (cur_n << 1) | 1);
-        make_all_n(i + 1, (cur_n << 1));
-    }
+int extrair_bit(int &mask) {
+    int ret = mask % 2;
+    mask /= 2;
+    return ret;
 }
 
-void solve(int i, int mi) {
-    // cout << i << ' ' << mi << '\n';
-    if(ans != -1) return;
-    if (i == M) {
-        // cout << "Mi: " << mi << '\n';
-        for (int ni : all_n) {
-            if (mi % ni == 0) {
-                ans = mi;
-                return;
-            }
-        }
-        return;
+bool verify(int mask) {
+    int mod = 0;
+    for (int i = 0; i < n.size(); i++) {
+        mod = mod * 2 + (n[i] == '*' ? extrair_bit(mask) : n[i] - '0');
+    }
+    int rest = 0, bit;
+    for (int i = 0; i < m.size(); i++) {
+        bit = (m[i] == '*' ? extrair_bit(mask) : m[i] - '0');
+        resp[i] = bit + '0';
+        rest = (rest * 2 + bit) % mod;
     }
 
-    if (m[i] != '*') solve(i + 1, (mi << 1) | (m[i] - '0'));
-    else {
-        solve(i + 1, (mi << 1) | 1);
-        solve(i + 1, (mi << 1));
-    }
+    return rest == 0;
 }
 
 int main(void) {
     cin >> m >> n;
-    M = (int) m.size();
-    N = (int) n.size();
-    make_all_n(0, 0);
-    solve(0, 0);
-    vector<short> ans_vec; 
-    while(ans) {
-        if (ans & 1) ans_vec.push_back(1);
-        else ans_vec.push_back(0);
-        ans = ans >> 1;
+    resp = m;
+
+    int cnt = 0;
+    for (int i = 0; i < m.size(); i++) {
+        if (m[i] == '*') cnt++;
     }
 
-    for (int i = (int) ans_vec.size() - 1; i >= 0; i--)
-        cout << ans_vec[i];
-    cout << '\n';
+    for (int i = 0; i < n.size(); i++) {
+        if (n[i] == '*') cnt++;
+    }
+
+    int lim_mask = 1 << cnt;
+
+    for (int mask = 0; mask < lim_mask; mask++) {
+        if (verify(mask)) {
+            cout << resp << '\n';
+            break; 
+        }
+    }
+
     return 0;
 }
