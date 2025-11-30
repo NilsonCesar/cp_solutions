96cbdd81cab1f3d040e0b355a635d5289bac1b02
nilsoncesar <admncoaf@gmail.com>
2025-11-30 20:27:07 -0300

    [final 2024] resolve j

--- maratona/2024/fase_final/j.cpp
+++ maratona/2024/fase_final/j.cpp
@@ -1,55 +1,40 @@
 #include <bits/stdc++.h>
- 
+
 using namespace std;
- 
-#define m_pi 3.14159265358979323846
- 
-int main(void) {
-    ios_base::sync_with_stdio(0);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    int t, n, u;
-    double x, h;
-    double ans = 0;
+
+#define _ cin.tie(NULL); ios_base::sync_with_stdio(false); cout.tie(NULL);
+#define INF 0x3f3f3f3f
+#define MAXN 100007
+
+double pi = acos(-1);
+
+vector<pair<double, int>> v;
+
+int main(void) {_
+    int t, n, x, h;
+    double len;
     cin >> t >> n;
-    vector<pair<double, double>> coords(n), x_axis;
-    vector<bool> to_ignore(n + 7, false);
-    map<int, double> p_to_x;
+
     cout << setprecision(15);
     for (int i = 0; i < n; i++) {
         cin >> x >> h;
-        coords[i].first = x;
-        coords[i].second = x + (h * 1.0) / (sin(t * m_pi / 180.00) / cos (t * m_pi / 180.00));
-    }
-    sort(coords.begin(), coords.end());
- 
-    for (int i = 0; i < n; i++) {
-        x_axis.push_back({coords[i].first, coords[i].second});
-        // cout << coords[i].first << ' ' << coords[i].second << '\n';
-    }
- 
-    sort(x_axis.begin(), x_axis.end());
-    double l = x_axis[0].first, r = x_axis[0].second;
-    int i = 1;
-    while (i < n) {
-        double ll = max(l, x_axis[i].first), rr = min(r, x_axis[i].second);
-        i++;
-        // cout << "here" << '\n';
-        // cout << l << ' ' << r  << ' ' << ans << '\n';
-        // cout << ll << ' ' << rr << '\n';
-        if (rr - ll == x_axis[i].second - x_axis[i].first) continue;
-        if (ll > rr) {
-            ans += r - l;
-            l = x_axis[i - 1].first;
-            r = x_axis[i - 1].second;
-        } else {
-            ans += ll - l;
-            l = x_axis[i - 1].first;
-            r = x_axis[i - 1].second;
+        len = h / tan(t * pi / 180);
+        v.push_back({x, 1});
+        v.push_back({x + len, -1});
+    }   
+    sort(v.begin(), v.end());
+
+    double ans = 0;
+    int cnt = 0;
+
+    for (int i = 0; i < (int) v.size(); i++) {
+        if (cnt > 0) {
+            ans += v[i].first - v[i - 1].first;
         }
+        cnt += v[i].second;
     }
 
-    cout << ans + r - l << '\n';
+    cout << ans << '\n';
 
     return 0;
 }
