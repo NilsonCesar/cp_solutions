b7659bc35cc97de3d285dba393c680c9332b07be
nilsoncesar <admncoaf@gmail.com>
2025-12-02 22:07:59 -0300

    [cses] solve maximum subarray sum

--- cses/sorting_and_searching/maximum_subarray_sum.cpp
+++ cses/sorting_and_searching/maximum_subarray_sum.cpp
@@ -7,6 +7,19 @@ using namespace std;
 #define MAXN 100007
 
 int main(void) {_
+    int n;
+    cin >> n;
+    int a;
+    long long ans = -INF, current_sum = 0;
+
+    for (int i = 0; i < n; i++) {
+        cin >> a;
+        current_sum += a;
+        ans = max(ans, current_sum);
+        if (current_sum < 0) current_sum = 0;
+    }
+
+    cout << ans << '\n';
 
     return 0;
 }
