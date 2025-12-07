30caebda2694557b37ab248261b8d6ca640c263e
nilsoncesar <admncoaf@gmail.com>
2025-12-07 19:17:32 -0300

    [cses] solve subarray divisibility

--- cses/sorting_and_searching/subarray_divisibility.cpp
+++ cses/sorting_and_searching/subarray_divisibility.cpp
@@ -12,19 +12,21 @@ void mainIO(string s = "") {
 int main(void) {
     mainIO();
     int n;
-    long long acc = 0, ans = 0;
+    long long acc = 0, ans = 0, a;
     cin >> n;
     vector<long long> sumpref(n, 0);
     sumpref[0] = 1;
     for(int i = 1; i <= n; i++) {
-        cin >> ans;
-        acc += ans;
+        cin >> a;
+        acc += a;
+
+        ans += sumpref[(acc % n + n) % n];
         sumpref[(acc % n + n) % n]++;
     }
  
-    ans = 0;
-    for(int i = 0; i < n; i++)
-        ans += sumpref[i] * (sumpref[i] - 1) / 2;
+    // ans = 0;
+    // for(int i = 0; i < n; i++)
+    //     ans += sumpref[i] * (sumpref[i] - 1) / 2;
  
     cout << ans << '\n';
  
